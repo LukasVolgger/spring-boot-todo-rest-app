3b4f92e4c541be3be89b930629fd21430b8cc543
Lukas Volgger <lukas.volgger@live.at>
2023-03-26 15:54:03 +0200

    first commit

---
+++ src/main/java/at/lukas/volgger/todorestapp/TodoRestAppApplication.java
@@ -0,0 +1,13 @@
+package at.lukas.volgger.todorestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/lukas/volgger/todorestapp/TodoRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package at.lukas.volgger.todorestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
