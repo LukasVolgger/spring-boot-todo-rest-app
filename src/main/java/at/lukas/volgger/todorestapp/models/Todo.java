52ad054e27bc828b890834b6b6d02509838936cc
Lukas Volgger <lukas.volgger@live.at>
2023-03-26 18:03:36 +0200

    Add Todo model

---
+++ src/main/java/at/lukas/volgger/todorestapp/models/Todo.java
@@ -0,0 +1,40 @@
+package at.lukas.volgger.todorestapp.models;
+
+public class Todo {
+    private Integer id;
+    private String title;
+    private String description;
+    private boolean isDone;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
+    }
+}
