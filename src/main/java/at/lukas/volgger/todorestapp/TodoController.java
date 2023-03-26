cd2a6f1a5e3bd5e1bb961b9b5fdcff535412958c
Lukas Volgger <lukas.volgger@live.at>
2023-03-26 17:45:41 +0200

    Add param to welcome method

--- src/main/java/at/lukas/volgger/todorestapp/TodoController.java
+++ src/main/java/at/lukas/volgger/todorestapp/TodoController.java
@@ -1,14 +1,15 @@
 package at.lukas.volgger.todorestapp;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TodoController {
 
     @GetMapping("/welcome")
-    public String welcome() {
-        return "Hello World!";
+    public String welcome(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "!";
     }
 
 }
